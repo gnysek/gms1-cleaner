288c7a02c08911779aa8e19f020904796abe517d
gnysek <gnysek@localhost>
2012-05-13 21:13:52 +0100

    inial commit with first release

---
+++ StudioCleaner/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace StudioCleaner
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.treeViewGMX = new System.Windows.Forms.TreeView();
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.butExpandAllDisk = new System.Windows.Forms.Button();
+			this.butExpandAllGMX = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnClearAll = new System.Windows.Forms.Button();
+			this.btnOpen = new System.Windows.Forms.Button();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.treeViewDisk = new System.Windows.Forms.TreeView();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.toolStripOpenDir = new System.Windows.Forms.ToolStripMenuItem();
+			this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusOrphans = new System.Windows.Forms.ToolStripStatusLabel();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.contextMenuStrip1.SuspendLayout();
+			this.panel4.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// treeViewGMX
+			// 
+			this.treeViewGMX.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeViewGMX.ImageIndex = 0;
+			this.treeViewGMX.ImageList = this.imageList1;
+			this.treeViewGMX.Location = new System.Drawing.Point(0, 0);
+			this.treeViewGMX.Name = "treeViewGMX";
+			this.treeViewGMX.SelectedImageIndex = 0;
+			this.treeViewGMX.Size = new System.Drawing.Size(432, 393);
+			this.treeViewGMX.TabIndex = 0;
+			// 
+			// imageList1
+			// 
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			this.imageList1.Images.SetKeyName(0, "folder.png");
+			this.imageList1.Images.SetKeyName(1, "file_extension_dll.png");
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.butExpandAllDisk);
+			this.panel1.Controls.Add(this.butExpandAllGMX);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.btnClearAll);
+			this.panel1.Controls.Add(this.btnOpen);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(853, 68);
+			this.panel1.TabIndex = 1;
+			// 
+			// butExpandAllDisk
+			// 
+			this.butExpandAllDisk.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.butExpandAllDisk.Image = global::StudioCleaner.Properties.Resources.folders_explorer;
+			this.butExpandAllDisk.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.butExpandAllDisk.Location = new System.Drawing.Point(550, 41);
+			this.butExpandAllDisk.Margin = new System.Windows.Forms.Padding(0);
+			this.butExpandAllDisk.Name = "butExpandAllDisk";
+			this.butExpandAllDisk.Size = new System.Drawing.Size(79, 24);
+			this.butExpandAllDisk.TabIndex = 5;
+			this.butExpandAllDisk.Text = "Expand All";
+			this.butExpandAllDisk.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.butExpandAllDisk.UseVisualStyleBackColor = true;
+			this.butExpandAllDisk.Click += new System.EventHandler(this.butExpandAllDisk_Click);
+			// 
+			// butExpandAllGMX
+			// 
+			this.butExpandAllGMX.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.butExpandAllGMX.Image = global::StudioCleaner.Properties.Resources.folders_explorer;
+			this.butExpandAllGMX.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.butExpandAllGMX.Location = new System.Drawing.Point(96, 41);
+			this.butExpandAllGMX.Margin = new System.Windows.Forms.Padding(0);
+			this.butExpandAllGMX.Name = "butExpandAllGMX";
+			this.butExpandAllGMX.Size = new System.Drawing.Size(79, 24);
+			this.butExpandAllGMX.TabIndex = 4;
+			this.butExpandAllGMX.Text = "Expand All";
+			this.butExpandAllGMX.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.butExpandAllGMX.UseVisualStyleBackColor = true;
+			this.butExpandAllGMX.Click += new System.EventHandler(this.butExpandAllGMX_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label2.Location = new System.Drawing.Point(468, 46);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(79, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Orphan files:";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label1.Location = new System.Drawing.Point(12, 46);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(81, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Project Files:";
+			// 
+			// btnClearAll
+			// 
+			this.btnClearAll.Enabled = false;
+			this.btnClearAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.btnClearAll.Image = global::StudioCleaner.Properties.Resources.bin;
+			this.btnClearAll.Location = new System.Drawing.Point(131, 3);
+			this.btnClearAll.Name = "btnClearAll";
+			this.btnClearAll.Size = new System.Drawing.Size(120, 38);
+			this.btnClearAll.TabIndex = 1;
+			this.btnClearAll.Text = "Clear All not incluced";
+			this.btnClearAll.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.btnClearAll.UseVisualStyleBackColor = true;
+			this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.btnOpen.Image = global::StudioCleaner.Properties.Resources.folder;
+			this.btnOpen.Location = new System.Drawing.Point(12, 3);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(113, 38);
+			this.btnOpen.TabIndex = 0;
+			this.btnOpen.Text = "Open GMX Project...";
+			this.btnOpen.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.treeViewGMX);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(432, 393);
+			this.panel2.TabIndex = 2;
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.treeViewDisk);
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panel3.Location = new System.Drawing.Point(432, 0);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(421, 393);
+			this.panel3.TabIndex = 3;
+			// 
+			// treeViewDisk
+			// 
+			this.treeViewDisk.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeViewDisk.ImageIndex = 0;
+			this.treeViewDisk.ImageList = this.imageList1;
+			this.treeViewDisk.Location = new System.Drawing.Point(0, 0);
+			this.treeViewDisk.Name = "treeViewDisk";
+			this.treeViewDisk.SelectedImageIndex = 0;
+			this.treeViewDisk.Size = new System.Drawing.Size(421, 393);
+			this.treeViewDisk.TabIndex = 0;
+			this.treeViewDisk.MouseUp += new System.Windows.Forms.MouseEventHandler(this.treeViewDisk_MouseUp);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.DefaultExt = "*.project.gmx";
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Filter = "GM:S Project|*.project.gmx";
+			this.openFileDialog1.SupportMultiDottedExtensions = true;
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripOpenDir,
+            this.deleteToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(176, 48);
+			// 
+			// toolStripOpenDir
+			// 
+			this.toolStripOpenDir.Image = global::StudioCleaner.Properties.Resources.folder_go;
+			this.toolStripOpenDir.Name = "toolStripOpenDir";
+			this.toolStripOpenDir.Size = new System.Drawing.Size(175, 22);
+			this.toolStripOpenDir.Text = "Open in directory...";
+			this.toolStripOpenDir.Click += new System.EventHandler(this.toolStripOpenDir_Click);
+			// 
+			// deleteToolStripMenuItem
+			// 
+			this.deleteToolStripMenuItem.Image = global::StudioCleaner.Properties.Resources.delete;
+			this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+			this.deleteToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+			this.deleteToolStripMenuItem.Text = "Delete";
+			this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+			// 
+			// panel4
+			// 
+			this.panel4.Controls.Add(this.panel3);
+			this.panel4.Controls.Add(this.panel2);
+			this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel4.Location = new System.Drawing.Point(0, 68);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(853, 393);
+			this.panel4.TabIndex = 5;
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusOrphans});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 437);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(853, 24);
+			this.statusStrip1.SizingGrip = false;
+			this.statusStrip1.TabIndex = 6;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(55, 19);
+			this.toolStripStatusLabel1.Text = "Oprhans:";
+			// 
+			// toolStripStatusOrphans
+			// 
+			this.toolStripStatusOrphans.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+			this.toolStripStatusOrphans.BorderStyle = System.Windows.Forms.Border3DStyle.RaisedInner;
+			this.toolStripStatusOrphans.Name = "toolStripStatusOrphans";
+			this.toolStripStatusOrphans.Size = new System.Drawing.Size(17, 19);
+			this.toolStripStatusOrphans.Text = "0";
+			this.toolStripStatusOrphans.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(853, 461);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "GMX cleaner (C) 2012 by gnysek";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel3.ResumeLayout(false);
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.panel4.ResumeLayout(false);
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TreeView treeViewGMX;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button btnClearAll;
+		private System.Windows.Forms.Button btnOpen;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.TreeView treeViewDisk;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem toolStripOpenDir;
+		private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+		private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button butExpandAllGMX;
+		private System.Windows.Forms.Button butExpandAllDisk;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusOrphans;
+	}
+}
+
---
+++ StudioCleaner/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Globalization;
+
+namespace StudioCleaner
+{
+	public partial class Form1 : Form
+	{
+		private string GMXfilename = "";
+		XmlDocument XMLfile;
+		int orphans = 0;
+		List<string> subFileNames = new List<string>();
+		List<string> subFilePaths = new List<string>();
+		List<TreeNode> subFileNodes = new List<TreeNode>();
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private string fup(string text)
+		{
+			return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text);
+		}
+
+		private void btnOpen_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.FileName = "";
+			DialogResult result = openFileDialog1.ShowDialog();
+
+			if (result == DialogResult.OK)
+			{
+				GMXfilename = openFileDialog1.FileName;
+				orphans = 0;
+
+				treeViewGMX.Nodes.Clear();
+				treeViewDisk.Nodes.Clear();
+				string PName = Path.GetFileNameWithoutExtension(Path.GetFileNameWithoutExtension(openFileDialog1.FileName));
+				treeViewGMX.Nodes.Add(PName);
+				treeViewDisk.Nodes.Add(PName);
+				readGMX(openFileDialog1.FileName);
+
+				treeViewDisk.ExpandAll();
+				toolStripStatusOrphans.Text = orphans.ToString();
+
+				btnClearAll.Enabled = (orphans > 0) ? true : false;
+
+				if (orphans == 0)
+				{
+					MessageBox.Show("Looks that this project is free of orphans! Yay!", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+			}
+		}
+
+		private void readGMX(string filename)
+		{
+			XMLfile = new XmlDocument();
+			XMLfile.Load(filename);
+
+			//list of GM resource types
+			string[] resTree = new string[] { "sprites", "sounds", "backgrounds", "paths", "fonts", "scripts", "objects", "rooms" };
+
+			foreach (string resType in resTree)
+			{
+				readGMXnode(filename, resType);
+			}
+
+		}
+
+		private void readGMXnode(string filename, string nodeName)
+		{
+			string nodeElementsName;
+			XmlNode root;
+
+			try
+			{
+				root = XMLfile.SelectSingleNode("assets/" + nodeName);
+
+				if (root == null)
+				{
+					return;
+				}
+
+				nodeElementsName = root.Attributes["name"].InnerText;
+
+				TreeNode main = treeViewGMX.Nodes.Add(fup(nodeElementsName));
+
+				readSubNode(root, nodeName, nodeElementsName, main);
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(nodeName + ": " + e.Message);
+			}
+
+			// compare with directory
+			try
+			{
+				string p;
+				p = Path.GetDirectoryName(filename) + "\\" + nodeName;
+				if (Directory.Exists(p))
+				{
+					TreeNode main = treeViewDisk.Nodes.Add(fup(nodeName));
+
+					foreach (string f in Directory.GetFiles(p, "*.gmx"))
+					{
+						string resName = Path.GetFileNameWithoutExtension(Path.GetFileNameWithoutExtension(f));
+
+						try
+						{
+							TreeNode selected = treeViewGMX.Nodes.Find(nodeName + "\\" + resName, true)[0];
+						}
+						catch
+						{
+							TreeNode current = main.Nodes.Add(Path.GetFileName(f), Path.GetFileName(f));
+							current.ForeColor = Color.Red;
+							current.ContextMenuStrip = contextMenuStrip1;
+							current.Tag = f;
+							current.ImageIndex = current.SelectedImageIndex = 1;
+							orphans++;
+						}
+					}
+
+					if (main.Nodes.Count < 1)
+					{
+						main.Remove();
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show(e.Message);
+			}
+		}
+
+		private void readSubNode(XmlNode node, string nodeName, string nodeElementsName, TreeNode main)
+		{
+			foreach (XmlNode n in node)
+			{
+				if (n.Attributes["name"] != null)
+				{
+					TreeNode sub = main.Nodes.Add(n.Attributes["name"].InnerText);
+					readSubNode(n, nodeName, nodeElementsName, sub);
+				}
+				else
+				{
+					TreeNode sub = main.Nodes.Add(n.InnerText, Path.GetFileName(n.InnerText));
+					sub.ImageIndex = sub.SelectedImageIndex = 1;
+				}
+			}
+		}
+
+		private void treeViewDisk_MouseUp(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Right)
+			{
+				Point pt = new Point(e.X, e.Y);
+				treeViewDisk.PointToClient(pt);
+
+				TreeNode Node = treeViewDisk.GetNodeAt(pt);
+				if (Node != null)
+				{
+					if (Node.Bounds.Contains(pt))
+					{
+						treeViewDisk.SelectedNode = Node;
+					}
+				}
+			}
+		}
+
+		private void toolStripOpenDir_Click(object sender, EventArgs e)
+		{
+			string filePath = @treeViewDisk.SelectedNode.Tag.ToString();
+			if (!File.Exists(filePath))
+			{
+				return;
+			}
+
+			System.Diagnostics.Process.Start("explorer.exe", @"/select, " + filePath);
+		}
+
+		private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			clearAllOprhansOnStack();
+
+			checkForSubOrphanFiles(treeViewDisk.SelectedNode);
+
+			if (!askAboutClearing(1)) return;
+
+			deleteAllOprhansOnStack();
+		}
+
+		private void deleteAllOprhansOnStack()
+		{
+			// remove subfiles
+			foreach (string f in subFilePaths)
+			{
+				File.Delete(f);
+			}
+
+			foreach (TreeNode t in subFileNodes)
+			{
+				File.Delete(t.Tag.ToString());
+
+				TreeNode parent = t.Parent;
+				t.Remove();
+				if (parent.Nodes.Count == 0) parent.Remove();
+				
+				orphans--;
+			}
+
+			if (orphans == 0)
+			{
+				btnClearAll.Enabled = false;
+			}
+
+			toolStripStatusOrphans.Text = orphans.ToString();
+
+			clearAllOprhansOnStack();
+		}
+
+		private void clearAllOprhansOnStack()
+		{
+			subFileNames.Clear();
+			subFilePaths.Clear();
+			subFileNodes.Clear();
+		}
+
+		private void butExpandAllGMX_Click(object sender, EventArgs e)
+		{
+			treeViewGMX.ExpandAll();
+		}
+
+		private void butExpandAllDisk_Click(object sender, EventArgs e)
+		{
+			treeViewDisk.ExpandAll();
+		}
+
+		/// <summary>
+		/// Clear all oprhan files
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnClearAll_Click(object sender, EventArgs e)
+		{
+			if (orphans == 0)
+			{
+				MessageBox.Show(
+					"No orphan files found",
+					Application.ProductName,
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Information
+				);
+				return;
+			}
+
+			clearAllOprhansOnStack();
+
+			foreach (TreeNode t in treeViewDisk.Nodes)
+			{
+				if (t.Nodes.Count > 0)
+				{
+					checkWholeTreeForCleaning(t);
+				}
+			}
+
+			if (!askAboutClearing(orphans)) return;
+
+			deleteAllOprhansOnStack();
+		}
+
+		private bool askAboutClearing(int howMany)
+		{
+			if (subFileNames.Count > 0)
+			{
+				if (DialogResult.No == MessageBox.Show(
+					"Removing those " + howMany.ToString() + " orphan(s) will also remove those files, are you sure?\n\n"
+					+ string.Join("\n", subFileNames.ToArray(), 0, Math.Min(subFileNames.Count, 10))
+					+ ((subFileNames.Count > 10) ? ("\n...\nand " + (subFileNames.Count-10).ToString() + " more")  : ""),
+					"Confirm",
+					MessageBoxButtons.YesNo,
+					MessageBoxIcon.Question
+				))
+				{
+					return false;
+				}
+			}
+			else
+			{
+				if (DialogResult.No == MessageBox.Show(
+					"This will delete " + howMany.ToString() + " oprhan(s). Are you sure? This operation will be permament!",
+					"Confirm",
+					MessageBoxButtons.YesNo,
+					MessageBoxIcon.Question
+				))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		private void checkWholeTreeForCleaning(TreeNode parent)
+		{
+			foreach (TreeNode t in parent.Nodes)
+			{
+				checkForSubOrphanFiles(t);
+			}
+		}
+
+		/// <summary>
+		/// Checks that any other file than GMX also needs to be removed
+		/// </summary>
+		/// <param name="t"></param>
+		private void checkForSubOrphanFiles(TreeNode t)
+		{
+			string filename = t.Tag.ToString();
+			if (!File.Exists(filename))
+			{
+				MessageBox.Show("File not found, skipped...\n" + filename, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+
+			checkForSubOrphanFilesByFilter(filename, ".sprite.gmx", "images", "_*");
+			checkForSubOrphanFilesByFilter(filename, ".background.gmx");
+			checkForSubOrphanFilesByFilter(filename, ".sound.gmx", "audio");
+
+			subFileNodes.Add(t);
+		}
+
+		/// <summary>
+		/// Checks for any files with same name like filename when filename contains proper filter, in subfolder, with additional sufix
+		/// </summary>
+		/// <param name="filename">current file filename</param>
+		/// <param name="filter">file extension filter</param>
+		/// <param name="subfolder">subfolder to search, default as images</param>
+		/// <param name="suffix">suffix added to filename without extension, default as *</param>
+		/// <returns></returns>
+		private bool checkForSubOrphanFilesByFilter(string filename, string filter, string subfolder = "images", string suffix = "*")
+		{
+			bool found = false;
+			if (filename.IndexOf(filter) > 0)
+			{
+				string dir = Path.GetDirectoryName(filename) + "\\" + subfolder + "\\";
+
+				foreach (string f in Directory.GetFiles(dir, Path.GetFileNameWithoutExtension(Path.GetFileNameWithoutExtension(filename)) + suffix))
+				{
+					subFileNames.Add(Path.GetFileName(f));
+					subFilePaths.Add(f);
+					found = true;
+				}
+			}
+
+			return found;
+		}
+	}
+}
