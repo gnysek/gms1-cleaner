206e4216f240f662eb91a3ea0eafadcabbecd7a5
gnysek <524094+gnysek@users.noreply.github.com>
2013-03-11 15:27:42 +0100

    - preparation for new format reading - fox for GMRes bool (whole class will be later removed)

---
+++ StudioCleaner/GMXFormat/GMXEnums.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudioCleaner.GMXFormat
+{
+	class GMXEnums
+	{
+	}
+
+	enum EventTypes {
+		Create,
+		Destroy,
+		Alarm,
+		Step,
+		Collision,
+		Keyboard,
+		Mouse,
+		Other,
+		Draw,
+		KeyPress,
+		KeyRelease,
+		Async,
+	}
+
+	public enum BoundingBoxTypes
+	{
+		Automatic,
+		Full,
+		Manual
+	}
+
+	public enum CollisionMaskShapes
+	{
+		Precise,
+		Rectangle,
+		Disk,
+		Diamond
+	}
+
+	public enum NodeTypes
+	{
+		Invalid = 0,
+		Root,
+		Group,
+		Child
+	}
+
+	public enum NodeGroup
+	{
+		Invalid = 0,
+		Objects,
+		Sprites,
+		Sounds,
+		Rooms,
+		Backgrounds = 6,
+		Scripts,
+		Paths,
+		Fonts,
+		TimeLines,
+		Includes,
+		Configs
+	}
+}
---
+++ StudioCleaner/GMXFormat/GMXFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudioCleaner.GMXFormat
+{
+	class GMXFile
+	{
+		public GMXFile() {
+			Sprites = new SpriteCollection();
+			Sound = new SoundCollection();
+			Backgrounds = new BackgroundCollection();
+			Paths = new PathCollection();
+			Scripts = new ScriptCollection();
+			Fonts = new FontCollection();
+			TimeLines = new TimeLineCollection();
+			Objects = new ObjectCollection();
+			Rooms = new RoomCollection();
+			Includes = new IncludedFileCollection();
+			Configs = new ConfigCollection();
+			ResourceTree = new ResourceTree();
+		}
+
+		public void readGMX() {
+		}
+
+		public SpriteCollection Sprites { get; private set; }
+		public SoundCollection Sound { get; private set; }
+		public BackgroundCollection Backgrounds { get; private set; }
+		public PathCollection Paths { get; private set; }
+		public ScriptCollection Scripts { get; private set; }
+		public FontCollection Fonts { get; private set; }
+		public TimeLineCollection TimeLines { get; private set; }
+		public ObjectCollection Objects{ get; private set; }
+		public RoomCollection Rooms { get; private set; }
+		public IncludedFileCollection Includes { get; private set; }
+		public ConfigCollection Configs { get; private set; }
+		public ResourceTree ResourceTree { get; private set; }
+	}
+}
