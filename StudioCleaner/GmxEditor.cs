7c2eebce00b136a00233f5b579f640cb3ca58e53
gnysek <524094+gnysek@users.noreply.github.com>
2013-04-23 16:28:52 +0200

    - gmeditor compontent created, but not yet used (lot of work here) - property form added as MDI (but will be changed to tabs maybe) - richXML removed from main form

---
+++ StudioCleaner/GmxEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StudioCleaner
+{
+	public partial class GmxEditor : UserControl
+	{
+		public List<string> lines = new List<string>();
+
+		public GmxEditor()
+		{
+			InitializeComponent();
+		}
+
+		private void GmxEditor_Paint(object sender, PaintEventArgs e)
+		{
+			ControlPaint.DrawBorder(e.Graphics, this.ClientRectangle, Color.Red, ButtonBorderStyle.Solid);
+		}
+	}
+}
---
+++ StudioCleaner/PropertyForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace StudioCleaner
+{
+	partial class PropertyForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.richXML = new System.Windows.Forms.RichTextBox();
+			this.gmxEditor1 = new StudioCleaner.GmxEditor();
+			this.SuspendLayout();
+			// 
+			// richXML
+			// 
+			this.richXML.BackColor = System.Drawing.Color.White;
+			this.richXML.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.richXML.DetectUrls = false;
+			this.richXML.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richXML.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.richXML.Location = new System.Drawing.Point(0, 0);
+			this.richXML.Name = "richXML";
+			this.richXML.ReadOnly = true;
+			this.richXML.Size = new System.Drawing.Size(347, 207);
+			this.richXML.TabIndex = 2;
+			this.richXML.Text = "";
+			this.richXML.WordWrap = false;
+			this.richXML.Enter += new System.EventHandler(this.richXML_Enter);
+			// 
+			// gmxEditor1
+			// 
+			this.gmxEditor1.Location = new System.Drawing.Point(292, 12);
+			this.gmxEditor1.Name = "gmxEditor1";
+			this.gmxEditor1.Size = new System.Drawing.Size(43, 42);
+			this.gmxEditor1.TabIndex = 1;
+			this.gmxEditor1.Visible = false;
+			// 
+			// PropertyForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(347, 207);
+			this.Controls.Add(this.gmxEditor1);
+			this.Controls.Add(this.richXML);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "PropertyForm";
+			this.Text = "PropertyForm";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private GmxEditor gmxEditor1;
+		public System.Windows.Forms.RichTextBox richXML;
+	}
+}
